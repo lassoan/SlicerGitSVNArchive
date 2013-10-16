5173d56d85a603b246c731b4c663b84343fd72e7
Andras Lasso <lasso@queensu.ca>
2013-10-16 17:11:22 -0400

    ENH: Always do lazyupdate for qMRMLSceneModel if scene is closing

--- Libs/MRML/Widgets/qMRMLSceneModel.cxx
+++ Libs/MRML/Widgets/qMRMLSceneModel.cxx
@@ -1130,7 +1130,7 @@ void qMRMLSceneModel::onMRMLSceneNodeAboutToBeRemoved(vtkMRMLScene* scene, vtkMR
   Q_UNUSED(scene);
   Q_ASSERT(scene == d->MRMLScene);
 
-  if (d->LazyUpdate && d->MRMLScene->IsBatchProcessing())
+  if (d->MRMLScene->IsClosing() || (d->LazyUpdate && d->MRMLScene->IsBatchProcessing()))
     {
     return;
     }
@@ -1183,7 +1183,7 @@ void qMRMLSceneModel::onMRMLSceneNodeRemoved(vtkMRMLScene* scene, vtkMRMLNode* n
   Q_D(qMRMLSceneModel);
   Q_UNUSED(scene);
   Q_UNUSED(node);
-  if (d->LazyUpdate && d->MRMLScene->IsBatchProcessing())
+  if (d->MRMLScene->IsClosing() || (d->LazyUpdate && d->MRMLScene->IsBatchProcessing()))
     {
     return;
     }
@@ -1282,7 +1282,7 @@ void qMRMLSceneModel::updateNodeItems(vtkMRMLNode* node, const QString& nodeUID)
 {
   Q_D(qMRMLSceneModel);
 
-  if (d->LazyUpdate && d->MRMLScene->IsBatchProcessing())
+  if (d->MRMLScene->IsClosing() || (d->LazyUpdate && d->MRMLScene->IsBatchProcessing()))
     {
     return;
     }
@@ -1413,11 +1413,7 @@ void qMRMLSceneModel::onMRMLSceneClosed(vtkMRMLScene* scene)
 {
   Q_D(qMRMLSceneModel);
   Q_UNUSED(scene);
-  //this->endResetModel();
-  if (d->LazyUpdate)
-    {
-    this->updateScene();
-    }
+  this->updateScene();
 }
 
 //------------------------------------------------------------------------------
