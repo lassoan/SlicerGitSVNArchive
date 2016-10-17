a1e6d89b689886c01b02c0d7f89321ee0767950f
Andras Lasso <lasso@queensu.ca>
2016-10-17 10:35:45 -0400

    ENH: Cleaned up vtkFastGrowCutSeg

--- Modules/Loadable/Segmentations/Logic/vtkFastGrowCutSeg.cxx
+++ Modules/Loadable/Segmentations/Logic/vtkFastGrowCutSeg.cxx
@@ -40,8 +40,8 @@ public:
   virtual int  operator <(FibHeapNode& RHS);
   virtual void operator =(double NewKeyVal);
   virtual void Print();
-  double GetKeyValue() { return N; }
-  void SetKeyValue(double n) { N = n; }
+  float GetKeyValue() { return N; }
+  void SetKeyValue(float n) { N = n; }
   long int GetIndexValue() { return IndexV; }
   void SetIndexValue(long int v) { IndexV = v; }
 };
@@ -98,28 +98,26 @@ public:
   template<typename SrcPixelType, typename LabPixelType>
   void DijkstraBasedClassificationAHP(vtkImageData *sourceVol, vtkImageData *seedVol);
 
-  template<typename SrcPixelType, typename LabPixelType>
-  bool DoSegmentation(vtkImageData *sourceVol, vtkImageData *seedVol);
-
   template <class SourceVolType>
-  void ExecuteGrowCut(vtkImageData *sourceVol, vtkImageData *seedVol, vtkImageData *outputVol);
+  bool ExecuteGrowCut(vtkImageData *sourceVol, vtkImageData *seedVol, vtkImageData *outputVol);
 
   template< class SourceVolType, class SeedVolType>
-  void ExecuteGrowCut2(vtkImageData *sourceVol, vtkImageData *seedVol);
-
-  void SetImageSize(const int imSize[3]);
+  bool ExecuteGrowCut2(vtkImageData *sourceVol, vtkImageData *seedVol);
 
   vtkSmartPointer<vtkImageData> m_imDist;
   vtkSmartPointer<vtkImageData> m_imDistPre;
   vtkSmartPointer<vtkImageData> m_imLab;
   vtkSmartPointer<vtkImageData> m_imLabPre;
 
-  int m_imSize[3];
-  long m_DIMX, m_DIMY, m_DIMZ, m_DIMXY, m_DIMXYZ;
-  std::vector<int> m_indOff;
+  long m_DIMX;
+  long m_DIMY;
+  long m_DIMZ;
+  long m_DIMXYZ;
+  std::vector<long> m_indOff;
   std::vector<unsigned char>  m_NBSIZE;
 
   FibHeap *m_heap;
+  //std::vector<HeapNode> m_hpNodes;
   HeapNode *m_hpNodes;
   bool m_bSegInitialized;
 };
@@ -150,6 +148,7 @@ void vtkFastGrowCutSeg::vtkInternal::Reset()
     delete m_heap;
     m_heap = NULL;
   }
+  //m_hpNodes.clear();
   if (m_hpNodes != NULL)
   {
     delete[]m_hpNodes;
@@ -164,37 +163,18 @@ void vtkFastGrowCutSeg::vtkInternal::Reset()
 
 //-----------------------------------------------------------------------------
 template<typename SrcPixelType, typename LabPixelType>
-bool vtkFastGrowCutSeg::vtkInternal::DoSegmentation(vtkImageData *sourceVol, vtkImageData *seedVol)
-{
-  if (!InitializationAHP<SrcPixelType, LabPixelType>(sourceVol, seedVol))
-  {
-    return false;
-  }
-  DijkstraBasedClassificationAHP<SrcPixelType, LabPixelType>(sourceVol, seedVol);
-  return true;
-};
-
-//-----------------------------------------------------------------------------
-template<typename SrcPixelType, typename LabPixelType>
 bool vtkFastGrowCutSeg::vtkInternal::InitializationAHP(vtkImageData *sourceVol, vtkImageData *seedVol)
 {
-  m_DIMX = m_imSize[0];
-  m_DIMY = m_imSize[1];
-  m_DIMZ = m_imSize[2];
-  m_DIMXY = m_DIMX*m_DIMY;
-  m_DIMXYZ = m_DIMXY*m_DIMZ;
-
-  if ((m_heap = new FibHeap) == NULL || (m_hpNodes = new HeapNode[m_DIMXYZ + 1]) == NULL)
+  m_heap = new FibHeap;
+  //m_hpNodes.resize(m_DIMXYZ + 1);
+  if ((m_hpNodes = new HeapNode[m_DIMXYZ + 1]) == NULL)
   {
-    vtkGenericWarningMacro("Memory allocation failed. Dimensions: " << m_imSize[0] << "x" << m_imSize[1] << "x" << m_imSize[2]);
+    vtkGenericWarningMacro("Memory allocation failed. Dimensions: " << m_DIMX << "x" << m_DIMY << "x" << m_DIMZ);
     return false;
   }
-  m_heap->ClearHeapOwnership();
-
   SrcPixelType* imSrc = static_cast<SrcPixelType*>(sourceVol->GetScalarPointer());
   LabPixelType* imSeed = static_cast<LabPixelType*>(seedVol->GetScalarPointer());
 
-  long  i, j, k, index;
   if (!m_bSegInitialized)
   {
     m_imLab->SetOrigin(seedVol->GetOrigin());
@@ -214,48 +194,55 @@ bool vtkFastGrowCutSeg::vtkInternal::InitializationAHP(vtkImageData *sourceVol, 
 
     // Compute index offset
     m_indOff.clear();
-    for (int ix = -1; ix <= 1; ix++)
+    for (int iz = -1; iz <= 1; iz++)
     {
       for (int iy = -1; iy <= 1; iy++)
       {
-        for (int iz = -1; iz <= 1; iz++)
+        for (int ix = -1; ix <= 1; ix++)
         {
-          if (!(ix == 0 && iy == 0 && iz == 0))
+          if (ix == 0 && iy == 0 && iz == 0)
           {
-            m_indOff.push_back(ix + iy*m_DIMX + iz*m_DIMXY);
+            continue;
           }
+          m_indOff.push_back(long(ix) + m_DIMX*(long(iy) + m_DIMY*long(iz)));
         }
       }
     }
 
     // Determine neighborhood size at each vertex
-    m_NBSIZE = std::vector<unsigned char>(m_DIMXYZ, 0);
-    for (i = 1; i < m_DIMX - 1; i++)
+    // (everywhere NNGBH except at the edges of the cube)
+    m_NBSIZE.resize(m_DIMXYZ);
+    unsigned char* nbSizePtr = &(m_NBSIZE[0]);
+    for (int z = 0; z < m_DIMZ; z++)
     {
-      for (j = 1; j < m_DIMY - 1; j++)
+      bool zEdge = (z == 0 || z == m_DIMZ - 1);
+      for (int y = 0; y < m_DIMY; y++)
       {
-        for (k = 1; k < m_DIMZ - 1; k++)
+        bool yEdge = (y == 0 || y == m_DIMY - 1);
+        *(nbSizePtr++) = 0; // x == 0 (there is always padding, so we don't need to check if m_DIMX>0)
+        unsigned char nbSize = (zEdge || yEdge) ? 0 : NNGBH;
+        for (int x = m_DIMX-2; x > 0; x--)
         {
-          index = i + j*m_DIMX + k*m_DIMXY;
-          m_NBSIZE[index] = NNGBH;
+          *(nbSizePtr++) = nbSize;
         }
+        *(nbSizePtr++) = 0; // x == m_DIMX-1 (there is always padding, so we don't need to check if m_DIMX>1)
       }
     }
 
-    for (index = 0; index < m_DIMXYZ; index++)
+    for (long index = 0; index < m_DIMXYZ; index++)
     {
-      imLab[index] = imSeed[index];
-      if (imLab[index] == 0)
+      LabPixelType seedValue = imSeed[index];
+      imLab[index] = seedValue;
+      if (seedValue == 0)
       {
-        m_hpNodes[index] = (float)DIST_INF;
+        m_hpNodes[index] = DIST_INF;
         imDist[index] = DIST_INF;
       }
       else
       {
-        m_hpNodes[index] = (float)DIST_EPSILON;
+        m_hpNodes[index] = DIST_EPSILON;
         imDist[index] = DIST_EPSILON;
       }
-
       m_heap->Insert(&m_hpNodes[index]);
       m_hpNodes[index].SetIndexValue(index);
     }
@@ -267,7 +254,7 @@ bool vtkFastGrowCutSeg::vtkInternal::InitializationAHP(vtkImageData *sourceVol, 
     LabPixelType* imLabPre = static_cast<LabPixelType*>(m_imLabPre->GetScalarPointer());
     DistPixelType* imDist = static_cast<DistPixelType*>(m_imDist->GetScalarPointer());
 
-    for (index = 0; index < m_DIMXYZ; index++)
+    for (long index = 0; index < m_DIMXYZ; index++)
     {
       if (imSeed[index] != 0 && imSeed[index] != imLabPre[index])
       {
@@ -293,17 +280,15 @@ bool vtkFastGrowCutSeg::vtkInternal::InitializationAHP(vtkImageData *sourceVol, 
 template<typename SrcPixelType, typename LabPixelType>
 void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData *sourceVol, vtkImageData *seedVol)
 {
-  HeapNode *hnMin, hnTmp;
-  float t, tOri, tSrc;
-  long i, index, indexNgbh;
-  LabPixelType labSrc;
-  SrcPixelType pixCenter;
-
   LabPixelType* imLab = static_cast<LabPixelType*>(m_imLab->GetScalarPointer());
   SrcPixelType* imSrc = static_cast<SrcPixelType*>(sourceVol->GetScalarPointer());
 
   // Insert 0 then extract it, which will balance heap
-  m_heap->Insert(&hnTmp); m_heap->ExtractMin();
+  {
+    HeapNode hnTmp;
+    m_heap->Insert(&hnTmp);
+    m_heap->ExtractMin();
+  }
 
   long k = 0; // it could be used for early termination of segmentation
 
@@ -312,19 +297,19 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
     LabPixelType* imLabPre = static_cast<LabPixelType*>(m_imLabPre->GetScalarPointer());
 
     // Adaptive Dijkstra
+    HeapNode hnTmp;
     while (!m_heap->IsEmpty())
     {
       DistPixelType* imDistPre = static_cast<DistPixelType*>(m_imDistPre->GetScalarPointer());
       DistPixelType* imDist = static_cast<DistPixelType*>(m_imDist->GetScalarPointer());
 
-      hnMin = (HeapNode *)m_heap->ExtractMin();
-      index = hnMin->GetIndexValue();
-      tSrc = hnMin->GetKeyValue();
+      HeapNode* hnMin = (HeapNode *)m_heap->ExtractMin();
+      float tSrc = hnMin->GetKeyValue();
 
-      // stop propagation when the new distance is larger than the previous one
+      // Stop of minimum value is infinite
       if (tSrc == DIST_INF)
       {
-        for (index = 0; index < m_DIMXYZ; index++)
+        for (long index = 0; index < m_DIMXYZ; index++)
         {
           if (imLab[index] == 0)
           {
@@ -334,6 +319,9 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
         }
         break;
       }
+
+      // Stop propagation when the new distance is larger than the previous one
+      long index = hnMin->GetIndexValue();
       if (tSrc > imDistPre[index])
       {
         imDist[index] = imDistPre[index];
@@ -341,16 +329,17 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
         continue;
       }
 
-      labSrc = imLab[index];
+      LabPixelType labSrc = imLab[index];
       imDist[index] = tSrc;
 
       // Update neighbors
-      pixCenter = imSrc[index];
-      for (i = 0; i < m_NBSIZE[index]; i++)
+      DistPixelType pixCenter = imSrc[index];
+      unsigned char nbSize = m_NBSIZE[index];
+      for (unsigned char i = 0; i < nbSize; i++)
       {
-        indexNgbh = index + m_indOff[i];
-        tOri = imDist[indexNgbh];
-        t = (pixCenter > imSrc[indexNgbh] ? pixCenter - imSrc[indexNgbh] : imSrc[indexNgbh] - pixCenter) + tSrc;
+        long indexNgbh = index + m_indOff[i];
+        float tOri = imDist[indexNgbh];
+        float t = fabs(pixCenter - imSrc[indexNgbh]) + tSrc;
         if (tOri > t)
         {
           imDist[indexNgbh] = t;
@@ -360,7 +349,6 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
           hnTmp.SetKeyValue(t);
           m_heap->DecreaseKey(&m_hpNodes[indexNgbh], hnTmp);
         }
-
       }
 
       k++;
@@ -372,21 +360,23 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
     LabPixelType* imLab = static_cast<LabPixelType*>(m_imLab->GetScalarPointer());
 
     // Normal Dijkstra (to be used in initializing the segmenter for the current image)
+    HeapNode hnTmp;
     while (!m_heap->IsEmpty())
     {
-      hnMin = (HeapNode *)m_heap->ExtractMin();
-      index = hnMin->GetIndexValue();
-      tSrc = hnMin->GetKeyValue();
-      labSrc = imLab[index];
+      HeapNode* hnMin = (HeapNode *)m_heap->ExtractMin();
+      long index = hnMin->GetIndexValue();
+      float tSrc = hnMin->GetKeyValue();
+      LabPixelType labSrc = imLab[index];
       imDist[index] = tSrc;
 
       // Update neighbors
-      pixCenter = imSrc[index];
-      for (i = 0; i < m_NBSIZE[index]; i++)
+      DistPixelType pixCenter = imSrc[index];
+      unsigned char nbSize = m_NBSIZE[index];
+      for (unsigned char i = 0; i < nbSize; i++)
       {
-        indexNgbh = index + m_indOff[i];
-        tOri = imDist[indexNgbh];
-        t = (pixCenter > imSrc[indexNgbh] ? pixCenter - imSrc[indexNgbh] : imSrc[indexNgbh] - pixCenter) + tSrc;
+        long indexNgbh = index + m_indOff[i];
+        float tOri = imDist[indexNgbh];
+        DistPixelType t = fabs(pixCenter - imSrc[indexNgbh]) + tSrc;
         if (tOri > t)
         {
           imDist[indexNgbh] = t;
@@ -412,6 +402,7 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
     delete m_heap;
     m_heap = NULL;
   }
+  //m_hpNodes.clear();
   if (m_hpNodes != NULL)
   {
     delete[]m_hpNodes;
@@ -420,53 +411,71 @@ void vtkFastGrowCutSeg::vtkInternal::DijkstraBasedClassificationAHP(vtkImageData
 }
 
 //-----------------------------------------------------------------------------
-template< class SourceVolType, class SeedVolType>
-void vtkFastGrowCutSeg::vtkInternal::ExecuteGrowCut2(vtkImageData *sourceVol, vtkImageData *seedVol)
+template< class SrcPixelType, class LabPixelType>
+bool vtkFastGrowCutSeg::vtkInternal::ExecuteGrowCut2(vtkImageData *sourceVol, vtkImageData *seedVol)
 {
-  // Do Segmentation
-  this->DoSegmentation<SourceVolType, SeedVolType>(sourceVol, seedVol);
+  int* imSize = sourceVol->GetDimensions();
+  m_DIMX = imSize[0];
+  m_DIMY = imSize[1];
+  m_DIMZ = imSize[2];
+  m_DIMXYZ = m_DIMX*m_DIMY*m_DIMZ;
+
+  if (m_DIMX <= 2 || m_DIMY <= 2 || m_DIMZ <= 2)
+  {
+    // image is too small (there should be space for at least one voxel padding around the image)
+    return false;
+  }
+
+  if (!InitializationAHP<SrcPixelType, LabPixelType>(sourceVol, seedVol))
+  {
+    return false;
+  }
+  DijkstraBasedClassificationAHP<SrcPixelType, LabPixelType>(sourceVol, seedVol);
+  return true;
 }
 
 //----------------------------------------------------------------------------
 template <class SourceVolType>
-void vtkFastGrowCutSeg::vtkInternal::ExecuteGrowCut(vtkImageData *sourceVol, vtkImageData *seedVol, vtkImageData *outData)
+bool vtkFastGrowCutSeg::vtkInternal::ExecuteGrowCut(vtkImageData *sourceVol, vtkImageData *seedVol, vtkImageData *outData)
 {
-
-  int extent[6];
-  double spacing[3], origin[3];
-  sourceVol->GetOrigin(origin);
-  sourceVol->GetSpacing(spacing);
-  sourceVol->GetExtent(extent);
-
-  int outExtent[6];
-  double outSpacing[3], outOrigin[3];
-  this->m_imLab->GetOrigin(outOrigin);
-  this->m_imLab->GetSpacing(outSpacing);
-  this->m_imLab->GetExtent(outExtent);
+  // Restart growcut from scratch if image size is changed (then cached buffers cannot be reused)
+  int* extent = sourceVol->GetExtent();
+  double* spacing = sourceVol->GetSpacing();
+  double* origin = sourceVol->GetOrigin();
+  int* outExtent = m_imLab->GetExtent();
+  double* outSpacing = m_imLab->GetSpacing();
+  double* outOrigin = m_imLab->GetOrigin();
   const double compareTolerance = 1e-6;
   if (outExtent[0] != extent[0] || outExtent[1] != extent[1]
     || outExtent[2] != extent[2] || outExtent[3] != extent[3]
     || outExtent[4] != extent[4] || outExtent[5] != extent[5]
-    || fabs(outOrigin[0] - origin[0])>compareTolerance
-    || fabs(outOrigin[1] - origin[1])>compareTolerance
-    || fabs(outOrigin[2] - origin[2])>compareTolerance
-    || fabs(outSpacing[0] - spacing[0])>compareTolerance
-    || fabs(outSpacing[1] - spacing[1])>compareTolerance
-    || fabs(outSpacing[2] - spacing[2])>compareTolerance)
+    || fabs(outOrigin[0] - origin[0]) > compareTolerance
+    || fabs(outOrigin[1] - origin[1]) > compareTolerance
+    || fabs(outOrigin[2] - origin[2]) > compareTolerance
+    || fabs(outSpacing[0] - spacing[0]) > compareTolerance
+    || fabs(outSpacing[1] - spacing[1]) > compareTolerance
+    || fabs(outSpacing[2] - spacing[2]) > compareTolerance)
   {
     this->Reset();
   }
 
-  sourceVol->GetDimensions(m_imSize);
-
+  bool success = false;
   switch (seedVol->GetScalarType())
   {
-    vtkTemplateMacro((ExecuteGrowCut2<SourceVolType, VTK_TT>(sourceVol, seedVol)));
+    vtkTemplateMacro((success = ExecuteGrowCut2<SourceVolType, VTK_TT>(sourceVol, seedVol)));
   default:
     vtkGenericWarningMacro("vtkOrientedImageDataResample::MergeImage: Unknown ScalarType");
   }
 
-  outData->ShallowCopy(this->m_imLab);
+  if (success)
+  {
+    outData->ShallowCopy(this->m_imLab);
+  }
+  else
+  {
+    outData->Initialize();
+  }
+  return success;
 }
 
 //-----------------------------------------------------------------------------
