lassoan/SlicerGitSVNArchive

commit d755f10a75f83dfe1f54aef308a70779a1b360ff

BUG: Extension build system: Sandbox download step and report download error
This commit updates the build system so that a failure in any of the
extension download/configure/build/test/packaging steps do not cause the
entire build to fail.

See https://discourse.slicer.org/t/no-nightly-mac-binary-since-august-1/836/10?u=jcfr

Fixes #4247

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26223 3bd1e089-480b-0410-8dfb-8563597acbee

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -7,12 +7,8 @@ set(expected_defined_vars
   QT_VERSION_MINOR
   Slicer_EXTENSIONS_TRACK_QUALIFIER
   Slicer_WC_REVISION
+  CTEST_DROP_SITE
   )
-if(Slicer_UPLOAD_EXTENSIONS)
-  list(APPEND expected_defined_vars
-    CTEST_DROP_SITE
-    )
-endif()
 foreach(var ${expected_defined_vars})
   if(NOT DEFINED ${var})
     message(FATAL_ERROR "Variable ${var} is not defined !")
@@ -47,10 +43,7 @@ foreach(varname
 endforeach()
 
 include(SlicerFunctionExtractExtensionDescription)
-
-if(Slicer_UPLOAD_EXTENSIONS)
-  include(SlicerBlockUploadExtensionPrerequisites) # Common to all extensions
-endif()
+include(SlicerBlockUploadExtensionPrerequisites) # Common to all extensions
 
 #-----------------------------------------------------------------------------
 # Collect extension description file (*.s4ext)
@@ -61,9 +54,10 @@ file(GLOB_RECURSE s4extfiles "${Slicer_EXTENSION_DESCRIPTION_DIR}/*.s4ext")
 set(EXTENSION_LIST)
 foreach(file ${s4extfiles})
   message(STATUS "Extension:${file}")
+
   # Extract extension description info
   slicerFunctionExtractExtensionDescription(EXTENSION_FILE ${file} VAR_PREFIX EXTENSION)
-  #message(DEPENDS:${EXTENSION_EXT_DEPENDS})
+
   # Extract file basename
   get_filename_component(EXTENSION_NAME ${file} NAME_WE)
   if("${EXTENSION_NAME}" STREQUAL "")
@@ -96,11 +90,6 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
   set(EXTENSION_ENABLED ${EXTENSION_EXT_ENABLED})
   set(EXTENSION_DEPENDS ${EXTENSION_EXT_DEPENDS})
 
-  #foreach(v SCM SCMURL SCMREVISION SVNUSERNAME SVNPASSWORD DEPENDS BUILD_SUBDIRECTORY HOMEPAGE
-  #          CATEGORY CONTRIBUTORS ICONURL STATUS DESCRIPTION SCREENSHOTURLS ENABLED)
-  #  message(${v}:${EXTENSION_EXT_${v}})
-  #endforeach()
-
   # Ensure extensions depending on this extension can lookup the corresponding
   # _DIR and _BUILD_SUBDIRECTORY variables.
   set(${EXTENSION_NAME}_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME}-build)
@@ -112,6 +101,29 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
     continue()
   endif()
 
+  # Set convenience variable
+  set(proj ${EXTENSION_NAME})
+
+  # Set external project DEPENDS parameter
+  set(EP_ARG_EXTENSION_DEPENDS)
+  if(Slicer_SOURCE_DIR)
+    set(EXTENSIONEP_ARG_EXTENSION_DEPENDS DEPENDS Slicer ${EXTENSION_DEPENDS})
+  else()
+    if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
+      set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_DEPENDS})
+    endif()
+  endif()
+  set(EXTENSION_REBUILD_DEPENDS)
+  if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
+    set(EP_ARG_EXTENSION_REBUILD_DEPENDS DEPENDS)
+    foreach(dep ${EXTENSION_DEPENDS})
+      list(APPEND EP_ARG_EXTENSION_REBUILD_DEPENDS ${dep}-rebuild)
+    endforeach()
+  endif()
+
+  #-----------------------------------------------------------------------------
+  # Configure extension source download wrapper script
+  #-----------------------------------------------------------------------------
   set(ext_ep_options_repository)
   set(ext_revision ${EXTENSION_EXT_SCMREVISION})
   if("${EXTENSION_EXT_SCM}" STREQUAL "git")
@@ -136,146 +148,113 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
          )
     endif()
   elseif("${EXTENSION_EXT_SCM}" STREQUAL "local")
-    set(ext_ep_options_repository DOWNLOAD_COMMAND "")
     set(EXTENSION_SOURCE_DIR ${EXTENSION_EXT_SCMURL})
     if(NOT IS_ABSOLUTE ${EXTENSION_SOURCE_DIR})
       set(EXTENSION_SOURCE_DIR ${Slicer_LOCAL_EXTENSIONS_DIR}/${EXTENSION_SOURCE_DIR})
     endif()
+    set(ext_ep_download_command DOWNLOAD_COMMAND "")
   else()
     message(WARNING "Unknown type of SCM [${EXTENSION_EXT_SCM}] associated with extension named ${EXTENSION_NAME} - See file ${file}")
     continue()
   endif()
-
-  # Set external project DEPENDS parameter
-  set(EP_ARG_EXTENSION_DEPENDS)
-  if(Slicer_SOURCE_DIR)
-    set(EXTENSIONEP_ARG_EXTENSION_DEPENDS DEPENDS Slicer ${EXTENSION_DEPENDS})
-  else()
-    if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
-      set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_DEPENDS})
-    endif()
-  endif()
-  set(EXTENSION_REBUILD_DEPENDS)
-  if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
-    set(EP_ARG_EXTENSION_REBUILD_DEPENDS DEPENDS)
-    foreach(dep ${EXTENSION_DEPENDS})
-      list(APPEND EP_ARG_EXTENSION_REBUILD_DEPENDS ${dep}-rebuild)
-    endforeach()
-  endif()
-  if(Slicer_UPLOAD_EXTENSIONS)
-
-    #-----------------------------------------------------------------------------
-    # Slicer_UPLOAD_EXTENSIONS: TRUE
-    #-----------------------------------------------------------------------------
-    set(EXTENSION_SUPERBUILD_BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR})
-    set(EXTENSION_BUILD_SUBDIRECTORY ${${EXTENSION_NAME}_BUILD_SUBDIRECTORY})
-    if(NOT DEFINED CTEST_MODEL)
-      set(CTEST_MODEL "Experimental")
-    endif()
-    include(SlicerBlockUploadExtension)
-
-    set(proj ${EXTENSION_NAME})
-
-    set(upload_extension_wrapper_script
-      ${CMAKE_CURRENT_BINARY_DIR}/upload_${proj}_wrapper_script.cmake)
-
+  if(NOT "${ext_ep_options_repository}" STREQUAL "")
     #
-    # The following wrapper script is required to workaround issue #4247
-    # and avoid the overall extension build from failing if only one test
-    # of an extension being depended on fails.
+    # The following wrapper script is required to avoid the overall extension
+    # build from failing if only one extension source tree failed to be
+    # downloaded.
     #
-    # Note that as soon as CMake >= 3.6.7 is released, it should be possible
-    # to remove the wrapper script and simply specify CAPTURE_CMAKE_ERROR
-    # ctest_test option.
-    #
-    # See https://cmake.org/cmake/help/v3.7/command/ctest_test.html
-    #
-    message(STATUS "Configuring extension upload wrapper script: ${upload_extension_wrapper_script}")
-    file(WRITE ${upload_extension_wrapper_script} "
+    # Add a "download" project allowing to retrieve the download command.
+    ExternalProject_Add(${proj}-download
+      ${ext_ep_options_repository}
+      SOURCE_DIR ${EXTENSION_SOURCE_DIR}
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      BUILD_IN_SOURCE 1
+      INSTALL_COMMAND ""
+      EXCLUDE_FROM_ALL 1
+      )
+    get_property(command TARGET ${proj}-download PROPERTY _EP_download_COMMAND)
+    set(download_extension_wrapper_script
+      ${CMAKE_CURRENT_BINARY_DIR}/download_${proj}_wrapper_script.cmake)
+    message(STATUS "Configuring extension download wrapper script: ${download_extension_wrapper_script}")
+    file(WRITE ${download_extension_wrapper_script} "
       execute_process(
-        COMMAND ${EXTENSION_UPLOAD_WRAPPER_COMMAND}
+        COMMAND ${command}
         WORKING_DIR \"${EXTENSION_SUPERBUILD_BINARY_DIR}\"
         RESULT_VARIABLE result
         )
-      message(STATUS \"upload_${proj}_wrapper_script: Ignoring result \${result}\")
+      message(STATUS \"download_${proj}_wrapper_script: Ignoring result \${result}\")
+      if(NOT result EQUAL 0)
+        message(STATUS \"Generating '${EXTENSION_SOURCE_DIR}/CMakeLists.txt'\")
+        file(MAKE_DIRECTORY \"${EXTENSION_SOURCE_DIR}\")
+        file(WRITE \"${EXTENSION_SOURCE_DIR}/CMakeLists.txt\"
+          \"cmake_minimum_required(VERSION 3.5)
+          project(${proj} NONE)
+          message(FATAL_ERROR \\\"Failed to download extension using ${ext_ep_options_repository}\\\")
+          \"
+          )
+      endif()
       ")
-
-    # Add extension external project
-    ExternalProject_Add(${proj}
-      ${ext_ep_options_repository}
-      SOURCE_DIR ${EXTENSION_SOURCE_DIR}
-      BINARY_DIR ${EXTENSION_SUPERBUILD_BINARY_DIR}
-      CONFIGURE_COMMAND ""
-      BUILD_COMMAND ${CMAKE_COMMAND} -DCTEST_BUILD_CONFIGURATION=${CTEST_BUILD_CONFIGURATION} -P ${upload_extension_wrapper_script}
-      INSTALL_COMMAND ""
-      ${EP_ARG_EXTENSION_DEPENDS}
-      )
-    # This custom external project step forces the build and later
-    # steps to run whenever a top level build is done...
-    ExternalProject_Add_Step(${proj} forcebuild
-      DEPENDEES configure
-      DEPENDERS build
-      ALWAYS 1
-      )
-
-  else()
-    #-----------------------------------------------------------------------------
-    # Slicer_UPLOAD_EXTENSIONS: FALSE
-    #-----------------------------------------------------------------------------
-    set(ext_ep_cmake_args
-      -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
-      -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE}
-      )
-    foreach(dep ${EXTENSION_DEPENDS})
-      list(APPEND ext_ep_cmake_args -D${dep}_DIR:PATH=${${dep}_BINARY_DIR}/${${dep}_BUILD_SUBDIRECTORY})
-    endforeach()
-
-    include(ListToString)
-    list_to_string("^^" "${EXTENSION_DEPENDS}" EXTENSION_DEPENDS)
-
-    list(APPEND ext_ep_cmake_args
-      -D${EXTENSION_NAME}_BUILD_SLICER_EXTENSION:BOOL=ON
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      -DCMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
-      -DCMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
-      -DBUILD_TESTING:BOOL=${BUILD_TESTING}
-      -DSlicer_DIR:PATH=${Slicer_DIR}
-      -DSlicer_EXTENSIONS_TRACK_QUALIFIER:STRING=${Slicer_EXTENSIONS_TRACK_QUALIFIER}
-      -DEXTENSION_BUILD_SUBDIRECTORY:STRING=${EXTENSION_EXT_BUILD_SUBDIRECTORY}
-      -DEXTENSION_ENABLED:BOOL=${EXTENSION_ENABLED}
-      -DEXTENSION_DEPENDS:STRING=${EXTENSION_DEPENDS}
-      -DMIDAS_PACKAGE_URL:STRING=${MIDAS_PACKAGE_URL}
-      -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
-      -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
-      )
-    if(APPLE)
-      list(APPEND ext_ep_cmake_args
-        -DCMAKE_OSX_ARCHITECTURES:STRING=${CMAKE_OSX_ARCHITECTURES}
-        -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
-        -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
-        )
-    endif()
-
-    # Add extension external project
-    set(proj ${EXTENSION_NAME})
-    ExternalProject_Add(${proj}
-      ${ext_ep_options_repository}
-      INSTALL_COMMAND ""
-      SOURCE_DIR ${EXTENSION_SOURCE_DIR}
-      BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR}
-      CMAKE_GENERATOR ${Slicer_EXTENSION_CMAKE_GENERATOR}
-      CMAKE_ARGS
-        ${ext_ep_cmake_args}
-      LIST_SEPARATOR "^^"
-      ${EP_ARG_EXTENSION_DEPENDS}
-      )
-    # This custom external project step forces the build and later
-    # steps to run whenever a top level build is done...
-    ExternalProject_Add_Step(${proj} forcebuild
-      DEPENDEES configure
-      DEPENDERS build
-      ALWAYS 1
+    set(ext_ep_download_command
+      DOWNLOAD_COMMAND ${CMAKE_COMMAND} -P ${download_extension_wrapper_script}
       )
   endif()
+
+  #-----------------------------------------------------------------------------
+  # Configure extension build wrapper script
+  #-----------------------------------------------------------------------------
+  set(EXTENSION_SUPERBUILD_BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR})
+  set(EXTENSION_BUILD_SUBDIRECTORY ${${EXTENSION_NAME}_BUILD_SUBDIRECTORY})
+  if(NOT DEFINED CTEST_MODEL)
+    set(CTEST_MODEL "Experimental")
+  endif()
+  include(SlicerBlockUploadExtension)
+  if(Slicer_UPLOAD_EXTENSIONS)
+    set(wrapper_command ${EXTENSION_UPLOAD_WRAPPER_COMMAND})
+  else()
+    set(wrapper_command ${EXTENSION_TEST_WRAPPER_COMMAND})
+  endif()
+
+  #
+  # The following wrapper script is required to avoid the overall extension
+  # build from failing if only one test of an extension being depended on fails.
+  # See #4247
+  #
+  # Note that as soon as CMake >= 3.6.7 is released, it should be possible
+  # to remove the wrapper script and simply specify CAPTURE_CMAKE_ERROR
+  # ctest_test option.
+  #
+  # See https://cmake.org/cmake/help/v3.7/command/ctest_test.html
+  #
+  set(build_extension_wrapper_script
+    ${CMAKE_CURRENT_BINARY_DIR}/build_${proj}_wrapper_script.cmake)
+  message(STATUS "Configuring extension upload wrapper script: ${build_extension_wrapper_script}")
+  file(WRITE ${build_extension_wrapper_script} "
+    execute_process(
+      COMMAND ${wrapper_command}
+      WORKING_DIR \"${EXTENSION_SUPERBUILD_BINARY_DIR}\"
+      RESULT_VARIABLE result
+      )
+    message(STATUS \"build_${proj}_wrapper_script: Ignoring result \${result}\")
+    ")
+
+  # Add extension external project
+  message("ext_ep_download_command:${ext_ep_download_command}")
+  ExternalProject_Add(${proj}
+    ${ext_ep_download_command}
+    SOURCE_DIR ${EXTENSION_SOURCE_DIR}
+    BINARY_DIR ${EXTENSION_SUPERBUILD_BINARY_DIR}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${CMAKE_COMMAND} -DCTEST_BUILD_CONFIGURATION=${CTEST_BUILD_CONFIGURATION} -P ${build_extension_wrapper_script}
+    INSTALL_COMMAND ""
+    ${EP_ARG_EXTENSION_DEPENDS}
+    )
+  # This custom external project step forces the build and later
+  # steps to run whenever a top level build is done...
+  ExternalProject_Add_Step(${proj} forcebuild
+    DEPENDEES configure
+    DEPENDERS build
+    ALWAYS 1
+    )
 
 endforeach()

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtension.cmake
@@ -19,6 +19,7 @@ set(expected_defined_vars
   BUILD_TESTING
   CTEST_BUILD_CONFIGURATION
   CTEST_CMAKE_GENERATOR
+  CTEST_DROP_SITE
   EXTENSION_BUILD_OPTIONS_STRING
   EXTENSION_BUILD_SUBDIRECTORY
   EXTENSION_ENABLED
@@ -39,7 +40,6 @@ set(expected_defined_vars
   )
 if(RUN_CTEST_UPLOAD)
   list(APPEND expected_defined_vars
-    CTEST_DROP_SITE
     EXTENSION_ARCHITECTURE
     EXTENSION_BITNESS
     EXTENSION_OPERATING_SYSTEM
@@ -231,13 +231,19 @@ if(build_errors GREATER "0")
 else()
   message("Packaging and uploading extension ${EXTENSION_NAME} to midas ...")
   set(package_list)
+  set(package_target "package")
+  if(RUN_CTEST_UPLOAD)
+    set(package_target "packageupload")
+  endif()
   if(RUN_CTEST_PACKAGES)
     ctest_build(
-      TARGET packageupload
+      TARGET ${package_target}
       BUILD ${EXTENSION_SUPERBUILD_BINARY_DIR}/${EXTENSION_BUILD_SUBDIRECTORY}
       APPEND
       )
-    ctest_submit(PARTS Build)
+    if(RUN_CTEST_SUBMIT)
+      ctest_submit(PARTS Build)
+    endif()
   endif()
 
   if(RUN_CTEST_UPLOAD)

Extensions/CMake/SlicerBlockUploadExtension.cmake
@@ -128,6 +128,7 @@ set(EXTENSION_COMMAND_BUILD_CONF_WRAPPER_ARG_LIST
 #-----------------------------------------------------------------------------
 # Set EXTENSION_TEST_COMMAND
 set(EXTENSION_TEST_COMMAND_ARG_LIST "${EXTENSION_COMMAND_ARG_LIST}
+set(CTEST_DROP_SITE \"${CTEST_DROP_SITE}\")
 set(RUN_CTEST_CONFIGURE \"${RUN_CTEST_CONFIGURE}\")
 set(RUN_CTEST_BUILD \"${RUN_CTEST_BUILD}\")
 set(RUN_CTEST_TEST \"${RUN_CTEST_TEST}\")
@@ -135,9 +136,11 @@ set(RUN_CTEST_PACKAGES \"${RUN_CTEST_PACKAGES}\")
 set(RUN_CTEST_UPLOAD \"FALSE\")")
 
 set(script_args_file ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME}-test-command-args.cmake)
-
 file(WRITE ${script_args_file} "${EXTENSION_TEST_COMMAND_ARG_LIST}")
-set(EXTENSION_TEST_COMMAND ${CMAKE_CTEST_COMMAND} ${EXTENSION_COMMAND_BUILD_CONF_ARG_LIST} -DCTEST_MODEL:STRING=${CTEST_MODEL} -DSCRIPT_ARGS_FILE:FILEPATH=${script_args_file} -S ${EXTENSION_SCRIPT} -V${CTEST_EXTRA_VERBOSE_ARG})
+
+set(EXTENSION_TEST_COMMAND_ARGS -DCTEST_MODEL:STRING=${CTEST_MODEL} -DSCRIPT_ARGS_FILE:FILEPATH=${script_args_file} -S ${EXTENSION_SCRIPT} -V${CTEST_EXTRA_VERBOSE_ARG})
+set(EXTENSION_TEST_COMMAND ${CMAKE_CTEST_COMMAND} ${EXTENSION_COMMAND_BUILD_CONF_ARG_LIST} ${EXTENSION_TEST_COMMAND_ARGS})
+set(EXTENSION_TEST_WRAPPER_COMMAND ${CMAKE_CTEST_COMMAND} ${EXTENSION_COMMAND_BUILD_CONF_WRAPPER_ARG_LIST} ${EXTENSION_TEST_COMMAND_ARGS})
 
 #-----------------------------------------------------------------------------
 # Set EXTENSION_UPLOAD_COMMAND

Extensions/CMake/Testing/SlicerExtensionBuildSystemTest.py
@@ -8,6 +8,7 @@ import re
 import string
 import subprocess
 import sys
+import textwrap
 import threading
 
 import BaseHTTPServer
@@ -332,6 +333,18 @@ class SlicerExtensionBuildSystemTest(unittest.TestCase):
           ]]
         self._remove_matching_lines(extension_dir + '/CMakeLists.txt', patterns)
 
+    # Add extension description file with invalid repository
+    description_file = extension_description_dir + '/TestExtInvalidSCM.s4ext'
+    print("Writing %s" % description_file)
+    with open(description_file, 'w') as description_file:
+      description_file.write(textwrap.dedent(
+        """
+        scm git
+        scmurl git://github.com/Slicer/ExtensionThatDoNotExists
+        scmrevision abcdefg
+        """))
+
+
   def test_index_build_with_upload(self):
     self._test_index_build('build_with_upload', True)
 
@@ -503,9 +516,9 @@ include({slicer_source_dir}/Extensions/CMake/SlicerExtensionsDashboardDriverScri
       cmd.extend(['-T', config.CMAKE_GENERATOR_TOOLSET])
 
     cmd.append('-DSlicer_UPLOAD_EXTENSIONS:BOOL=%s' % ('1' if test_upload else '0'))
+    cmd.append('-DCTEST_DROP_SITE:STRING=%s' % self.ctest_drop_site)
 
     if test_upload:
-      cmd.append('-DCTEST_DROP_SITE:STRING=%s' % self.ctest_drop_site)
       cmd.append('-DMIDAS_PACKAGE_URL:STRING=%s' % self.midas_package_url)
 
     cmd.append(config.Slicer_SOURCE_DIR + '/Extensions/CMake')
@@ -597,41 +610,55 @@ include({slicer_source_dir}/Extensions/CMake/SlicerExtensionsDashboardDriverScri
 
     requests = iter(_requests)
 
+    # Check number of requests
+    expected_request_count = 0
+
+    # Top-level configure/build/notes
+    if with_ctest:
+      expected_request_count += 3
+
+    # Configure/Build/Test/Packaging for TestExtA, TestExtB and TestExtC
+    expected_request_count += 4 * 3
+
+    # Configure for TestExtInvalidSCM
+    expected_request_count += 1
+
+    # Upload to Midas and CDash for TestExtA, TestExtB and TestExtC
+    if test_upload:
+      expected_request_count += 3 * 3
+
+    self.assertEqual(len(_requests), expected_request_count)
+
     # Check CDash and Midas queries
-    if not test_upload:
-      self.assertEqual(len(_requests), 3 if with_ctest else 0)
+    if with_ctest:
+      # Upload top-level configure results to CDash
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Configure\.xml')
 
-      if with_ctest:
-        # Upload top-level configure results to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Configure\.xml')
-        # Upload top-level build results and notes to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Build\.xml')
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Notes\.xml')
+    for extensionName in ['TestExtInvalidSCM', 'TestExtA', 'TestExtB', 'TestExtC']:
 
-    else:
-      self.assertEqual(len(_requests), 7 * 3 + (3 if with_ctest else 0))
+      # Upload configure/build/test results to CDash
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Configure\.xml')
+      if extensionName == 'TestExtInvalidSCM':
+        continue
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Build\.xml')
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Test\.xml')
 
-      if with_ctest:
-        # Upload top-level configure results to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Configure\.xml')
-
-      for extensionName in ['TestExtA', 'TestExtB', 'TestExtC']:
-        # Upload configure/build/test results to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Configure\.xml')
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Build\.xml')
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Test\.xml')
+      if test_upload:
         # Upload package to midas
         check_midas_request(parse_request(requests.next()), 'GET', 'midas.login')
         check_midas_request(parse_request(requests.next()), 'PUT', 'midas.slicerpackages.extension.upload', {'productname': extensionName})
-        # Upload packaging result to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Build\.xml')
+
+      # Upload packaging result to CDash
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Build\.xml')
+
+      if test_upload:
         # Upload url to CDash
         check_cdash_request(parse_request(requests.next()), 'PUT', r'.+' + extensionName + r'.+Upload\.xml')
 
-      if with_ctest:
-        # Upload top-level build results and notes to CDash
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Build\.xml')
-        check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Notes\.xml')
+    if with_ctest:
+      # Upload top-level build results and notes to CDash
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Build\.xml')
+      check_cdash_request(parse_request(requests.next()), 'PUT', r'.+Notes\.xml')
 
   def _check_midas_upload_query_parameters(self, test_upload, with_ctest=False):
     if not test_upload:

commit 751ca57a5fa94b8a5b213474fe9ae48a486ca7f5

BUG: Extension build system: Fix checkout of SVN based extensions
This commit fixes the current working directory associated with SVN
command. The regression was introduced in r26223 (BUG: Extension build
system: Sandbox download step and report download error).

See https://issues.slicer.org/view.php?id=4417

Thanks: Andriy Fedorov <fedorov@bwh.harvard.edu>
Thanks: Csaba Pinter <csaba.pinter@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26291 3bd1e089-480b-0410-8dfb-8563597acbee

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -188,7 +188,7 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
     file(WRITE ${download_extension_wrapper_script} "
       execute_process(
         COMMAND ${command}
-        WORKING_DIR \"${EXTENSION_SUPERBUILD_BINARY_DIR}\"
+        WORKING_DIRECTORY \"${CMAKE_CURRENT_BINARY_DIR}\"
         RESULT_VARIABLE result
         )
       message(STATUS \"download_${proj}_wrapper_script: Ignoring result \${result}\")